970387a2af5f3fc2fa85ee5be862f6a521a52d4f
YasinRabiee <hlww6303@gmail.com>
2025-10-05 12:19:00 -0700

    fix bugs

---
+++ controllers/User.php
@@ -0,0 +1,18 @@
+<?php
+	# Load controllers class
+	spl_autoload_register(function ($class)
+	{
+		require_once __DIR__."/models/$class.php";
+	});
+	use eftec\bladeone\BladeOne;
+
+	class User
+	{
+		private $blade;
+
+		public function __construct()
+		{
+			$this->blade = new BladeOne(__DIR__.'/../views', __DIR__.'/../cache', BladeOne::MODE_AUTO);
+		}
+	}
+?>
